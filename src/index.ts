d915657b352bad26d908846c9a0ddd36e8deb480
Chris <chrisbreuer93@gmail.com>
2024-11-08 14:54:39 +0100

    chore: default to dtsx defaults

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import type { DtsGenerationOption } from '@stacksjs/dtsx'
 import type { BunPlugin } from 'bun'
-import process from 'node:process'
 import { generate } from '@stacksjs/dtsx'
 
 export function dts(options?: DtsGenerationOption): BunPlugin {
@@ -8,12 +7,12 @@ export function dts(options?: DtsGenerationOption): BunPlugin {
     name: 'bun-plugin-dtsx',
 
     async setup(build) {
-      const cwd = options?.cwd || process.cwd()
+      const cwd = options?.cwd
       const root = options?.root || build.config.root
       const entrypoints = options?.entrypoints // || build.config.entrypoints - we are not resorting to this yet because the `bundle` dtsx option is not yet supported
       const outdir = options?.outdir || build.config.outdir
-      const clean = options?.clean || false
-      const tsconfigPath = options?.tsconfigPath || './tsconfig.json'
+      const clean = options?.clean
+      const tsconfigPath = options?.tsconfigPath
       // const keepComments = options?.keepComments || true
 
       await generate({
